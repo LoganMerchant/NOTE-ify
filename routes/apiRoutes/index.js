04a95015f0d594cb58d1bc4ba998be5eafd1765e
Logan Merchant <merchantclogan@gmail.com>
2020-10-09 03:15:50 -0600

    add delete endpoint, uuid for note ids

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const router = require('express').Router();
 const db = require('../../db/db.json');
 
@@ -10,6 +11,7 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
     const newNote = req.body;
 
+    newNote.id = uuidv4();
     db.push(newNote);
 
     fs.writeFileSync(
@@ -18,6 +20,26 @@ router.post('/notes', (req, res) => {
     );
 
     res.json(db);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const noteToDelete = req.params.id;
+    console.log(noteToDelete);
+
+    db.forEach((note, index) => {
+        if (note.id === noteToDelete) {
+            console.log('i got here at least');
+            
+            db.splice(index, 1);
+
+            fs.writeFileSync(
+                path.join(__dirname, '../../db/db.json'),
+                JSON.stringify(db, null, 2),
+            );
+        };
+    });
+
+    res.send(`Note deleted!`);
 });
 
 module.exports = router;
