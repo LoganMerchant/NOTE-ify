74ad48f0bc82f181657ec9e83fa5ddc7d88914e0
Logan Merchant <merchantclogan@gmail.com>
2020-10-11 19:26:22 -0600

    add tests for creation and deletion of notes

--- __tests__/route-functions.test.js
+++ __tests__/route-functions.test.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+const db = require('../db/db.json');
 const { createNote, findById } = require("../lib/route-functions.js");
 
-test('')
+jest.mock('fs');
+
+test('checks that a new note is being created', () => {
+    const dbLength = db.length;
+    
+    createNote(
+        { title: "TEST", text: "TEST" },
+        db
+    );
+
+    expect(db).toHaveLength(dbLength + 1);
+});
+
+test('checks that a note is being deleted', () => {
+    const dbLength = db.length;
+
+    db.push({ "title": "TITLE", "text": "TEXT", "id": "TEST ID" });
+    expect(db).toHaveLength(dbLength + 1);
+
+    findById(db, "TEST ID");
+    expect(db).toHaveLength(dbLength);
+});
